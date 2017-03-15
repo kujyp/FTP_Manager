4f716464bc038aef36e735e880ccf4173cecc1d8
pjo901018 <pjo901018@korea.ac.kr>
2017-03-15 18:53:45 +0900

    모듈로 사용하기위해 함수화

--- downloadfromftp.py
+++ downloadfromftp.py
@@ -9,60 +9,61 @@ import sys
 import time
 
 
-# encoding for filename with korean
-reload(sys)
-sys.setdefaultencoding('utf-8')
+def downloadfromftp():
+    # encoding for filename with korean
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
 
-# load config.py
-PARMS = Parameter()
-ftp_domain = PARMS.ftp_domain
-ftp_user = PARMS.ftp_user
-ftp_pwd = PARMS.ftp_pwd
-ftp_homepath = PARMS.ftp_homepath
-ftp_targetpath = PARMS.ftp_targetpath
-ftp_downloadlocaldir = convert_path_sep(PARMS.ftp_downloadlocaldir)
+    # load config.py
+    PARMS = Parameter()
+    ftp_domain = PARMS.ftp_domain
+    ftp_user = PARMS.ftp_user
+    ftp_pwd = PARMS.ftp_pwd
+    ftp_homepath = PARMS.ftp_homepath
+    ftp_targetpath = PARMS.ftp_targetpath
+    ftp_downloadlocaldir = convert_path_sep(PARMS.ftp_downloadlocaldir)
 
-# mkdir in local
-local_dir = ftp_downloadlocaldir
-localtools.mkdir_unless_exist(ftp_downloadlocaldir)
+    # mkdir in local
+    local_dir = ftp_downloadlocaldir
+    localtools.mkdir_unless_exist(ftp_downloadlocaldir)
 
-# ftp connect
-ftp = FTP(ftp_domain)
-print('Connecting FTP Server : \t\t' + ftp_domain)
-ftp.login(ftp_user,ftp_pwd)
+    # ftp connect
+    ftp = FTP(ftp_domain)
+    print('Connecting FTP Server : \t\t' + ftp_domain)
+    ftp.login(ftp_user,ftp_pwd)
 
-ftp_path = (ftp_homepath + '/' + ftp_targetpath)
-print('Connecting FTP Dir : \t\t' + ftp_path)
-ftp_relpaths = get_everyrelpath_fromftp(ftp, ftp_path)
+    ftp_path = (ftp_homepath + '/' + ftp_targetpath)
+    print('Connecting FTP Dir : \t\t' + ftp_path)
+    ftp_relpaths = get_everyrelpath_fromftp(ftp, ftp_path)
 
-# Download all files
-pbar = tqdm(ftp_relpaths)
-for ftp_relpath in pbar:
-    filename = ftp_relpath.split('/')[-1]
-    dirnames = ftp_relpath.split('/')[1:-1]
+    # Download all files
+    pbar = tqdm(ftp_relpaths)
+    for ftp_relpath in pbar:
+        filename = ftp_relpath.split('/')[-1]
+        dirnames = ftp_relpath.split('/')[1:-1]
 
-    ftp_curpath = ftp_path
-    local_path = local_dir
-    for dirname in dirnames:
-        ftp_curpath = ftp_curpath + '/' + dirname
-        local_path = os.path.join(local_path, dirname)
+        ftp_curpath = ftp_path
+        local_path = local_dir
+        for dirname in dirnames:
+            ftp_curpath = ftp_curpath + '/' + dirname
+            local_path = os.path.join(local_path, dirname)
 
-    local_pathwithfilename = os.path.join(local_path, filename)
-    if os.path.isfile(local_pathwithfilename):
-        pbar.set_description('Exist file : \t\t\t\t' + local_pathwithfilename + '\n')
-    else:
-        try:
-            localtools.mkdir_unless_exist(local_path)
-            ftp.cwd(ftp_curpath)
-            pbar.set_description("Downloading ... \t\t\t" + local_pathwithfilename + '\n')
-            ftp.retrbinary('RETR %s' % filename, open(local_pathwithfilename, 'wb').write)
-        except:
-            pbar.set_description("Downloading ... \t\t\t" + local_pathwithfilename + ' retry . ' + '\n')
-            time.sleep(1)
-            pbar.set_description("Downloading ... \t\t\t" + local_pathwithfilename + ' retry .. ' + '\n')
-            time.sleep(1)
-            pbar.set_description("Downloading ... \t\t\t" + local_pathwithfilename + ' retry ... ' + '\n')
-            time.sleep(1)
-            ftp.retrbinary('RETR %s' % filename, open(local_pathwithfilename, 'wb').write)
+        local_pathwithfilename = os.path.join(local_path, filename)
+        if os.path.isfile(local_pathwithfilename):
+            pbar.set_description('Exist file : \t\t\t\t' + local_pathwithfilename + '\n')
+        else:
+            try:
+                localtools.mkdir_unless_exist(local_path)
+                ftp.cwd(ftp_curpath)
+                pbar.set_description("Downloading ... \t\t\t" + local_pathwithfilename + '\n')
+                ftp.retrbinary('RETR %s' % filename, open(local_pathwithfilename, 'wb').write)
+            except:
+                pbar.set_description("Downloading ... \t\t\t" + local_pathwithfilename + ' retry . ' + '\n')
+                time.sleep(1)
+                pbar.set_description("Downloading ... \t\t\t" + local_pathwithfilename + ' retry .. ' + '\n')
+                time.sleep(1)
+                pbar.set_description("Downloading ... \t\t\t" + local_pathwithfilename + ' retry ... ' + '\n')
+                time.sleep(1)
+                ftp.retrbinary('RETR %s' % filename, open(local_pathwithfilename, 'wb').write)
 
-ftp.quit()
+    ftp.quit()
--- uploadtoftp.py
+++ uploadtoftp.py
@@ -9,78 +9,79 @@ import sys
 import time
 
 
-# encoding for filename with korean
-reload(sys)
-sys.setdefaultencoding('utf-8')
+def uploadtoftp():
+    # encoding for filename with korean
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
 
-# load config.py
-PARMS = Parameter()
-ftp_domain = PARMS.ftp_domain
-ftp_user = PARMS.ftp_user
-ftp_pwd = PARMS.ftp_pwd
-ftp_homepath = PARMS.ftp_homepath
-ftp_targetpath = PARMS.ftp_targetpath
-ftp_uploadlocaldir = convert_path_sep(PARMS.ftp_uploadlocaldir)
+    # load config.py
+    PARMS = Parameter()
+    ftp_domain = PARMS.ftp_domain
+    ftp_user = PARMS.ftp_user
+    ftp_pwd = PARMS.ftp_pwd
+    ftp_homepath = PARMS.ftp_homepath
+    ftp_targetpath = PARMS.ftp_targetpath
+    ftp_uploadlocaldir = convert_path_sep(PARMS.ftp_uploadlocaldir)
 
-def load_allpath(path):
-    res = []
+    def load_allpath(path):
+        res = []
 
-    for root, dirs, files in os.walk(path):
-        for file in files:
-           filepath = os.path.join(root, file)
-           res.append(filepath)
-    return res
+        for root, dirs, files in os.walk(path):
+            for file in files:
+               filepath = os.path.join(root, file)
+               res.append(filepath)
+        return res
 
-def file_exists_inftp(ftp_paths,filename):
-    for ftp_path in ftp_paths:
-        if ftp_path == filename:
-            return True
-    return False
+    def file_exists_inftp(ftp_paths,filename):
+        for ftp_path in ftp_paths:
+            if ftp_path == filename:
+                return True
+        return False
 
-# ftp connect
-ftp = FTP(ftp_domain)
-ftp.login(ftp_user,ftp_pwd)
-print('Connected FTP Server : \t\t' + ftp_domain)
+    # ftp connect
+    ftp = FTP(ftp_domain)
+    ftp.login(ftp_user,ftp_pwd)
+    print('Connected FTP Server : \t\t' + ftp_domain)
 
-# existing FTP files
-ftp_path = (ftp_homepath + '/' + ftp_targetpath)
-ftp_relpaths = get_everyrelpath_fromftp(ftp, ftp_path)
-print('Connected FTP Dir : \t\t' + ftp_path)
+    # existing FTP files
+    ftp_path = (ftp_homepath + '/' + ftp_targetpath)
+    ftp_relpaths = get_everyrelpath_fromftp(ftp, ftp_path)
+    print('Connected FTP Dir : \t\t' + ftp_path)
 
-local_paths = load_allpath(ftp_uploadlocaldir)
-local_abspathlen = len(ftp_uploadlocaldir)
+    local_paths = load_allpath(ftp_uploadlocaldir)
+    local_abspathlen = len(ftp_uploadlocaldir)
 
-pbar = tqdm(local_paths)
-for local_path in pbar:
-    filename = local_path.split(os.sep)[-1]
-    dirnames = local_path.split(os.sep)[1:-1]
+    pbar = tqdm(local_paths)
+    for local_path in pbar:
+        filename = local_path.split(os.sep)[-1]
+        dirnames = local_path.split(os.sep)[1:-1]
 
-    ftp_curpath = ftp_path
-    for dirname in dirnames:
-        ftp_curpath= ftp_curpath + '/' + dirname
+        ftp_curpath = ftp_path
+        for dirname in dirnames:
+            ftp_curpath= ftp_curpath + '/' + dirname
 
-    # ignore .DS_Store
-    if filename[0] == '.':
-        continue
+        # ignore .DS_Store
+        if filename[0] == '.':
+            continue
 
-    local_file = open(local_path,'rb')
-    local_relpath = local_path[local_abspathlen:]
+        local_file = open(local_path,'rb')
+        local_relpath = local_path[local_abspathlen:]
 
-    if file_exists_inftp(ftp_relpaths,local_relpath):
-        pbar.set_description('Exist file : \t\t\t\t' + local_relpath + '\n')
-    else:
-        try:
-            ftptools.mkdir_unless_exist(ftp, ftp_curpath)
-            ftp.cwd(ftp_curpath)
-            pbar.set_description('Uploading ... \t\t\t\t' + local_relpath + '\n')
-            ftp.storbinary('STOR %s' % filename, local_file)
-        except:
-            pbar.set_description('Uploading ... \t\t\t\t' + local_relpath + ' retry . ' + '\n')
-            time.sleep(1)
-            pbar.set_description('Uploading ... \t\t\t\t' + local_relpath + ' retry .. ' + '\n')
-            time.sleep(1)
-            pbar.set_description('Uploading ... \t\t\t\t' + local_relpath + ' retry ... ' + '\n')
-            time.sleep(1)
-            ftp.storbinary('STOR %s' % filename, local_file)
+        if file_exists_inftp(ftp_relpaths,local_relpath):
+            pbar.set_description('Exist file : \t\t\t\t' + local_relpath + '\n')
+        else:
+            try:
+                ftptools.mkdir_unless_exist(ftp, ftp_curpath)
+                ftp.cwd(ftp_curpath)
+                pbar.set_description('Uploading ... \t\t\t\t' + local_relpath + '\n')
+                ftp.storbinary('STOR %s' % filename, local_file)
+            except:
+                pbar.set_description('Uploading ... \t\t\t\t' + local_relpath + ' retry . ' + '\n')
+                time.sleep(1)
+                pbar.set_description('Uploading ... \t\t\t\t' + local_relpath + ' retry .. ' + '\n')
+                time.sleep(1)
+                pbar.set_description('Uploading ... \t\t\t\t' + local_relpath + ' retry ... ' + '\n')
+                time.sleep(1)
+                ftp.storbinary('STOR %s' % filename, local_file)
 
-ftp.quit()
+    ftp.quit()
